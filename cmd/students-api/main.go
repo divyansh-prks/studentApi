7c80c75f56718b999dee1a3d891760a5fce5ad20
divyansh <divyansh2375@gmail.com>
2025-02-09 11:10:41 +0530

    gracefully shutting down the server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/divyansh/students-api/internal/config"
 )
@@ -27,13 +33,36 @@ func main(){
 		Handler: router,
 	}
 
+
+    slog.Info("server started" , slog.String("address" , cfg.Addr))
 	fmt.Printf("Server is started %s" , cfg.HTTPServer.Addr)
-	err := server.ListenAndServe()
+
+	done := make(chan os.Signal , 1)
+
+	signal.Notify(done , os.Interrupt , syscall.SIGINT , syscall.SIGTERM)
+
+	go func(){
+		err := server.ListenAndServe()
 
 	if err != nil {
 		log.Fatal("failed to start the server")
 	}
 
+
+	}()
+
+	<-done
+	
+	slog.Info("Shutting down the server")
+
+	ctx , cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+
+	defer cancel()
+	err := server.Shutdown(ctx)
+
+	if err != nil {
+		slog.Error("failed to shutdown the server" , slog.String("error" , err.Error()))
+	}
 
 
 
