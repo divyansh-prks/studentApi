3faa22d4bc0dc06b69f35fdf570154784e3e4ffb
divyansh <divyansh2375@gmail.com>
2025-02-09 10:09:11 +0530

    config path must set

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,40 @@
-package main 
-import "fmt"
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/divyansh/students-api/internal/config"
+)
 
 func main(){
-	fmt.Print("Hello world")
+
+	//load config 
+
+	cfg := config.MustLoad()
+	//database setup 
+	//setup router 
+	router := http.NewServeMux()
+	router.HandleFunc("GET /" ,func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("welcome to students api"))
+
+	})
+	//setup server
+
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server is started %s" , cfg.HTTPServer.Addr)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("failed to start the server")
+	}
+
+
+
 
 } 
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,14 +9,14 @@ import (
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr string `yaml:"address" env-required:"true"`
 }
 
 // env-default:"production"
 type Config struct {
 	Env string `yaml:"env" env:"ENV" env-required:"true" `
-	StoragePath string
-	HTTPServer
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer `yaml:"http_server"`
 
 }
 
